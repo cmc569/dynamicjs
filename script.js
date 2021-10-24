967a2f4d8464c8eeffe0ff351a5c1267c9223b3a
CMC <cmc569@gmail.com>
2021-10-24 11:33:56 +0800

    initial upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let s_html = '<h1>Hello, from script js</h1>';
+
+function aaa() {
+    alert('alert from script.js');
+}
---
+++ scriptB.js
@@ -0,0 +1,5 @@
+let s_html = '<h1>Hello, from scriptB js</h1>';
+
+function aaa() {
+    alert('alert from scriptB.js');
+}
